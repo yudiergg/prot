81ca785a0a6328f93a8534ad0f4c47ac63b9866f
yudiergg <gkscks1248@gmail.com>
2025-01-27 22:29:01 +0900

    .
    .

--- Aerok/js/main.js
+++ Aerok/js/main.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 2500,
-		disableOnInteraction: true,
-	},
-
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 100000000000,
+			disableOnInteraction: true,
 		},
-	},
 	
-
-	navigation: {  /* 이전, 다음 버튼 */
-		nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-		prevEl: '.swiper-button-prev',  
-	},
-
-});
-swiper.autoplay.stop();  /* 일시정지 기능 */
-swiper.autoplay.start();  /* 재생 기능 */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.swiper-pagination', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+			type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+			renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+				return '<span class="' + className + '">' + (index + 1) + "</span>";
+			},
+		},
+		});
+		// console.log('함수실행')
 });
