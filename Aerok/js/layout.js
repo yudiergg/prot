4f40083d32a19b780d916920ceb46b9289e87ef6
yudiergg <gkscks1248@gmail.com>
2025-01-30 23:27:42 +0900

    event 만드는중
    ..

--- Aerok/js/layout.js
+++ Aerok/js/layout.js
@@ -9,15 +9,20 @@ $(document).ready(function () {
             $(this).removeClass('open'); // 현재 li에서 .open 제거
         }
     );
-        // 언어 버튼 클릭 시 언어 목록 표시/숨김
-        $(".lang button").click(function () {
-          $(".lang ul").toggle().css("display", "flex");
-        });
+    $('.lang button').click(function (e) {
+        e.stopPropagation(); // 클릭 이벤트 버블링을 막아 부모 요소로 전달되지 않도록 함
+        $('.lang ul').toggle().css('display', 'flex');
+      });
     
-        // 메뉴 클릭 시 active 클래스 추가
-        $(".tnb ul li a").click(function (e) {
-          e.preventDefault(); // 기본 링크 동작 방지
-          $(".tnb ul li").removeClass("active"); // 기존 active 제거
-          $(this).parent().addClass("active"); // 현재 클릭된 요소에 추가
-        });
+      // 메뉴 클릭 시 active 클래스 추가
+      $('.tnb ul li a').click(function (e) {
+        e.preventDefault(); // 기본 링크 동작 방지
+        $('.tnb ul li').removeClass('active'); // 기존 active 제거
+        $(this).parent().addClass('active'); // 현재 클릭된 요소에 추가
+      });
+    
+      // header에서 마우스가 떠나면 .lang ul 숨기기
+      $('header').mouseleave(function () {
+        $(".lang ul").hide(); // 마우스가 header 밖으로 나가면 .lang ul 숨김
+      });
 });
