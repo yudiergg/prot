a6291183d8cb7c5fb7b03fb995e4a683c7f8ad9c
yudiergg <gkscks1248@gmail.com>
2025-01-31 20:31:56 +0900

    event tiket list
    button navgation 구현해야함

--- Aerok/js/main.js
+++ Aerok/js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 			// },
 		},
 		});
+
 		$(".tab_menu li a").click(function (e) {
 			e.preventDefault();
 			
@@ -26,6 +27,32 @@ $(document).ready(function(){
 			$(".tab_content section").hide();
 			$($(this).attr("href")).show();
 		});
-	
+
 		$("#section2").hide(); // 페이지 로드 시 section2 숨김
+
+		const event_ticket_swiper = new Swiper('.event_ticket .swiper', { /* 팝업을 감싼는 요소의 class명 */
+			slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+			spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+			breakpoints: {
+				640: {    /* 640px 이상일때 적용 */
+					slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+					spaceBetween: 48,
+				},
+			},
+			centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+			loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+			autoplay: {  /* 팝업 자동 실행 */
+				delay: 1000000,
+				disableOnInteraction: false,
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+				el: '.event_ticket .swiper-pagination', /* 해당 요소의 class명 */
+				clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+				type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+			},
+		});
 });
