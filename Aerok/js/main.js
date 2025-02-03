78b6ad4fe25c6d54e700b49d75e2653cf5986262
yudiergg <gkscks1248@gmail.com>
2025-02-03 18:01:28 +0900

    수정해야할꺼 푸터 헤더 수정하면 끝
    ..

--- Aerok/js/main.js
+++ Aerok/js/main.js
@@ -31,12 +31,16 @@ $(document).ready(function(){
 		$("#section2").hide(); // 페이지 로드 시 section2 숨김
 
 		const event_ticket_swiper = new Swiper('.event_ticket .swiper', { /* 팝업을 감싼는 요소의 class명 */
-			slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-			spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+			slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+			spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
 			breakpoints: {
 				640: {    /* 640px 이상일때 적용 */
 					slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
 					spaceBetween: 48,
+				},
+				758: {    /* 640px 이상일때 적용 */
+					slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+					spaceBetween: 24,
 				},
 			},
 			centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
@@ -51,24 +55,41 @@ $(document).ready(function(){
 			},
 			pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
 				el: '.event_ticket .swiper-pagination', /* 해당 요소의 class명 */
-				clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+				clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지s 값 */
 				type: 'bullets',  /* type fraction을 주면 paging이 숫자로 표시됨 */
 			},
 		});
+
 		gsap.registerPlugin(ScrollTrigger);
-		const sections =  document.querySelector(".section");  //좌우요소를 감싸는 요소
-		const large =  document.querySelector(".section .cont_wrap .cont"); //스크롤될 요소
-		gsap.to(large, {
-			y: () => (window.innerHeight - large.clientHeight - 64),  /* 실제 스크롤 값보다 더 스크롤 할 값 - 필요없으면 0 */
-			ease: "none",
-			scrollTrigger: {
-				trigger: sections,
-				pin: true,
-				start: "top 20px", /* 좌우로 스크롤 될 동안의 위치, top top 상단에 고정, top 20% 상단에서 20% 떨어져서 */
-				end: () => "+=500",
-				scrub: 0.5, 
-				markers: false,
-				invalidateOnRefresh: true,
+
+		function initScrollTrigger() {
+			// 기존 ScrollTrigger 제거 (초기화)
+			ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+		
+			if (window.innerWidth > 768) {
+				// 768px 초과에서만 실행
+				const news_sections = document.querySelector('.news .wrapper .section');  
+				const large = document.querySelector('.news .wrapper .section .cont_wrap .cont');
+		
+				gsap.to(large, {
+					y: () => (window.innerHeight - large.clientHeight - 0), 
+					ease: "none",
+					scrollTrigger: {
+						trigger: news_sections,
+						pin: true,
+						start: "top 20%",
+						end: () => "+=500",
+						scrub: 0.5,
+						markers: false,
+						invalidateOnRefresh: false,
+					}
+				});
 			}
-		});
+		}
+		
+		// 초기 실행
+		initScrollTrigger();
+		
+		// 화면 크기 변경 시 다시 적용
+		window.addEventListener("resize", initScrollTrigger);
 });
