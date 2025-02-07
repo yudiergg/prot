97015dda8fbdad10bfc1f935adfb40e9bd5adf54
yudiergg <gkscks1248@gmail.com>
2025-02-07 11:42:14 +0900

    250207-모바일 반응형 코딩
    ..

--- Aerok/js/main.js
+++ Aerok/js/main.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
 		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 		autoplay: {  /* 팝업 자동 실행 */
-			delay: 2500,
+			delay: 10000,
 			disableOnInteraction: false,
 		},
 		navigation: {
