308d1587a6d605de44d8588520117d782fc83acb
yudiergg <gkscks1248@gmail.com>
2025-02-13 22:54:29 +0900

    ..
    ..

--- rot/js/main.js
+++ rot/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	breakpoints: {
 		640: {    /* 640px 이상일때 적용 */
 			slidesPerView: 1,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 20,
+			spaceBetween: 0,
 		},
 	},
 
