9d68cdb1c8aaac1a95dac5991a9bb73899a4ab95
yudiergg <gkscks1248@gmail.com>
2025-02-20 18:01:47 +0900

    250220.scss
    시청꺼 하는중

--- Sejong/js/main.js
+++ Sejong/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     const visual_popup_swiper = new Swiper('.visual_popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
 	autoplay: {  /* 팝업 자동 실행 */
-		delay: 2500,
+		delay: 100000,
 		disableOnInteraction: true,
 	},
 
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
 	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.new_item .swiper-pagination', /* 해당 요소의 class명 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
 		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
 		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
 		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
@@ -29,5 +29,8 @@ $(document).ready(function(){
 		nextEl: '.next',  
 		prevEl: '.prev',  
 	},
-}); 
+
+});
+swiper.autoplay.stop();  /* 일시정지 기능 */
+swiper.autoplay.start();  /* 재생 기능 */
 })
