8e2c0e903ed5f6898bd28508cf3cf84f4ce8140a
yudiergg <gkscks1248@gmail.com>
2025-02-24 18:03:05 +0900

    250224-반응형
    ..

--- Sejong/js/main.js
+++ Sejong/js/main.js
@@ -31,6 +31,37 @@ $(document).ready(function(){
 	},
 
 });
-swiper.autoplay.stop();  /* 일시정지 기능 */
-swiper.autoplay.start();  /* 재생 기능 */
+
+/*swiper.autoplay.stop();  
+swiper.autoplay.start();
+*/
+
+/* 팝업존 */
+
+
+const popzone_wrap_swiper = new Swiper('.popzone_wrap .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		640: {    /* 640px 이상일때 적용 */
+			slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 24,
+		},
+	},
+	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 100000,
+		disableOnInteraction: true,
+	},
+	navigation: {
+		nextEl: '.popzone_wrap .swiper_nav .prev',
+		prevEl: '.popzone_wrap .swiper_nav .next',
+	},
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+	},
+});
 })
