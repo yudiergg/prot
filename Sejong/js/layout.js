bae837274131c69cd19de68dcb5444ebc247adb4
yudiergg <gkscks1248@gmail.com>
2025-02-24 21:42:24 +0900

    250224-헤더메뉴함
    ..

---
+++ Sejong/js/layout.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.header_bottom .gnb ul.depth1 li').on('mouseenter', function(){
+        $(this).addClass('active'); // li에 active 클래스 추가
+        $(this).find('.lower_menu').stop().css('display', 'flex').hide().fadeIn(300);
+    }).on('mouseleave', function(){
+        $(this).find('.lower_menu').stop().fadeOut(300, function(){
+            $(this).css('display', 'none')
+        })
+        $(this).removeClass('active') // li에서 active 클래스 제거
+    })
+})
