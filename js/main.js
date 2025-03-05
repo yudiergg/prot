2e313f5ea37da7142673b5751273a48d9c4cb326
yudiergg <gkscks1248@gmail.com>
2025-03-05 09:56:27 +0900

    250305-스위퍼 사이즈 일정하게 고정시켜줌
    ..

--- js/main.js
+++ js/main.js
@@ -6,14 +6,8 @@ $(document).ready(function(){
 		disableOnInteraction: true,
 	},
 
-    slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-	breakpoints: {
-		640: {    /* 640px 이상일때 적용 */
-			slidesPerView: 5,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 24,
-		},
-	},
+    slidesPerView: 5, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
 
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
