d74158003be2f930c92e09066214fe358c0a95de
yudiergg <gkscks1248@gmail.com>
2025-08-28 12:16:37 +0900

    1.1
    링크 수정 카운트 및 html 구조 변경

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function(){
-    const project_popup_swiper = new Swiper('.project_popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
+// $(document).ready(function(){
+//     const project_popup_swiper = new Swiper('.project_popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 2500,
-		disableOnInteraction: true,
-	},
+// 	autoplay: {  /* 팝업 자동 실행 */
+// 		delay: 2500,
+// 		disableOnInteraction: true,
+// 	},
 
-    slidesPerView: 5, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
+//     slidesPerView: 5, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+// 	spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
 
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+// 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
-		},
-	},	
-	navigation: {  
-		nextEl: '.next',  
-		prevEl: '.prev',  
-	},
-});
+// 	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+// 		el: '.swiper-pagination', /* 해당 요소의 class명 */
+// 		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+// 		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+// 		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+// 		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+// 		},
+// 	},	
+// 	navigation: {  
+// 		nextEl: '.next',  
+// 		prevEl: '.prev',  
+// 	},
+// });
 
-gsap.from(".tit", {duration: 5, text: ""}) //글자를 감싸는 요소
-})
+// gsap.from(".tit", {duration: 5, text: ""}) //글자를 감싸는 요소
+// })
